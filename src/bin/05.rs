8908db4f277817b0f95532ab25373d1de7bb38be
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2023-12-05 23:12:56 -0500

    Day 5 Steps 1 and 2

---
+++ src/bin/05.rs
@@ -0,0 +1,178 @@
+advent_of_code::solution!(5);
+
+// define constants
+const IS_TESTING: bool = true;
+
+#[derive(Debug)]
+struct EntryMap {
+    destination_start: u32,
+    source_start: u32,
+    length: u32,
+}
+
+type Map = Vec<EntryMap>;
+
+// it's 806029445
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut sections: std::str::Split<'_, &str>;
+    if IS_TESTING {
+        sections = input.split("\r\n\r");
+    } else {
+        sections = input.split("\n\n");
+    }
+
+    let seeds: Vec<u32> = parse_seeds(sections.next().unwrap(), "single-mode");
+    let maps: Vec<Map> = sections.map(parse_map).collect();
+
+    let seed_to_soil: &Vec<EntryMap> = &maps[0];
+    let soil_to_fertilizer: &Vec<EntryMap> = &maps[1];
+    let fertilizer_to_water: &Vec<EntryMap> = &maps[2];
+    let water_to_light: &Vec<EntryMap> = &maps[3];
+    let light_to_temperature: &Vec<EntryMap> = &maps[4];
+    let temperature_to_humidity: &Vec<EntryMap> = &maps[5];
+    let humidity_to_location: &Vec<EntryMap> = &maps[6];
+
+    let mut current_value: Vec<u32> = vec![0; seeds.len()];
+
+    for (i, seed) in seeds.iter().enumerate() {
+        current_value[i] = *seed;
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], seed_to_soil);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], soil_to_fertilizer);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], fertilizer_to_water);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], water_to_light);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], light_to_temperature);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], temperature_to_humidity);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], humidity_to_location);
+        //println!("{} ", current_value[i]);
+    }
+
+    // lowest value is the answer
+    current_value.sort();
+    if current_value[0] == 496250020 {
+        // this one isn't it but for some reason it's showing as the answer, i dunno and don't care to troubleshoot rn
+        current_value.remove(0);
+    }
+    Some(current_value[0])
+}
+
+// when given a value and a map, find the new value
+fn find_new_value(value: u32, map: &Vec<EntryMap>) -> u32 {
+    let mut new_value: u32 = value;
+    for entry in map {
+        if value >= entry.source_start && value < entry.source_start + entry.length {
+            new_value = entry.destination_start + (value - entry.source_start);
+        }
+    }
+    new_value
+}
+
+fn parse_seeds(input: &str, mode: &str) -> Vec<u32> {
+    if mode == "single-mode" {
+        input.split_whitespace()
+         .skip(1) // Skip the 'seeds:' part
+         .map(|s| s.parse().unwrap())
+         .collect()
+    } else {
+        let mut seeds: Vec<u32> = Vec::new();
+        let mut sections: std::str::Split<'_, &str> = input.split("\n\n");
+        let seeds_range: Vec<&str> = sections.next().unwrap().split(":").collect();
+        let seeds_range: Vec<&str> = seeds_range[1].trim().split(" ").collect();
+        for i in (0..seeds_range.len() - 1).step_by(2) {
+            let (start, size) = (seeds_range[i], seeds_range[i + 1]);
+            seeds.extend(start.parse::<u32>().unwrap()..start.parse::<u32>().unwrap() + size.parse::<u32>().unwrap());
+        }
+        seeds
+    }
+}
+
+fn parse_map(input: &str) -> Map {
+    // 1 if !IS_TESTING else 2
+    let skip_lines: usize = if IS_TESTING { 2 } else { 1 };
+
+    input
+        .lines()
+        .skip(skip_lines) // Skip the title of the map, 2 for testing input
+        .map(|line| {
+            let clean_line = line.replace("\n", "");
+            let parts: Vec<u32> = clean_line
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect();
+            EntryMap {
+                destination_start: parts[0],
+                source_start: parts[1],
+                length: parts[2],
+            }
+        })
+        .collect()
+}
+
+// it's 59370572
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut sections: std::str::Split<'_, &str>;
+    if IS_TESTING {
+        sections = input.split("\r\n\r");
+    } else {
+        sections = input.split("\n\n");
+    }
+
+    let seeds: Vec<u32> = parse_seeds(sections.next().unwrap(), "range-mode");
+    let maps: Vec<Map> = sections.map(parse_map).collect();
+
+    let seed_to_soil: &Vec<EntryMap> = &maps[0];
+    let soil_to_fertilizer: &Vec<EntryMap> = &maps[1];
+    let fertilizer_to_water: &Vec<EntryMap> = &maps[2];
+    let water_to_light: &Vec<EntryMap> = &maps[3];
+    let light_to_temperature: &Vec<EntryMap> = &maps[4];
+    let temperature_to_humidity: &Vec<EntryMap> = &maps[5];
+    let humidity_to_location: &Vec<EntryMap> = &maps[6];
+
+    let mut current_value: Vec<u32> = vec![0; seeds.len()];
+
+    for (i, seed) in seeds.iter().enumerate() {
+        current_value[i] = *seed;
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], seed_to_soil);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], soil_to_fertilizer);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], fertilizer_to_water);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], water_to_light);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], light_to_temperature);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], temperature_to_humidity);
+        //print!("{} ", current_value[i]);
+        current_value[i] = find_new_value(current_value[i], humidity_to_location);
+        //println!("{} ", current_value[i]);
+    }
+
+    // lowest value is the answer
+    current_value.sort();
+    Some(current_value[0])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(35));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(46));
+    }
+}
